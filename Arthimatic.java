c29ec4606b94495f063c10d567c7ebb10632a93f
RachanaKNS <rachanarachukokale@gmail.com>
2023-08-17 22:45:07 +0530

    Operators
    Operators

---
+++ Arthimatic.java
@@ -0,0 +1,17 @@
+class Arthimatic
+{
+	public static void main(String[] args)
+	{
+		int a = 10,b = 20;
+		int sum = a + b;
+		int difference = a - b;
+		int product = a * b;
+		int quoitent = a / b;
+		int modulus = a % b;
+		System.out.println("Addition =" + sum);
+		System.out.println("Sub =" + difference);
+		System.out.println("Mul =" + product);
+		System.out.println("Div =" + quoitent);
+		System.out.println("Modulus =" + modulus);
+	}
+}
---
+++ Comparision.java
@@ -0,0 +1,12 @@
+class Comparision
+{
+public static void main(String[] args)
+{
+System.out.println(7 > 5);
+System.out.println(3 >= 9);
+System.out.println(9 < 6);
+System.out.println(9 <= 12);
+System.out.println(1 == 2);
+System.out.println(0 != 5);
+}
+}
